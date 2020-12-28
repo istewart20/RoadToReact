e03041935d88c4ea78c9c88f29f1fd77e82047a9
Iain Stewart <iain.stewart.08@gmail.com>
2020-12-28 23:58:11 +0000

    Display welcome object and search box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 function App() {
+  const welcome = {
+    greeting: 'Hey',
+    title: 'React'
+  }
+
   return (
   <div>
-  <h1>Hello World</h1>
+    <h1>{welcome.greeting} {welcome.title}</h1>
+
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
   </div>
   );
 }
